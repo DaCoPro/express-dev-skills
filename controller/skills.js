ea191664000c80facc4cae35f41b41e6e8fbe6ff
David Comer <davidcomer@Davids-MacBook-Pro.local>
2020-11-23 13:35:54 -0800

    Remove removal and addition features. Clean up views.

--- controller/skills.js
+++ controller/skills.js
@@ -1,40 +1,15 @@
-//pull in the skills data
 const Skill = require('../models/skill');
 
-//This is the information we're pushing out to be used by routes
 module.exports = {
     index,
     show,
-    new: newTodo,
-    create,
-    delete: deleteTodo
 }
 
-//delete function
-function deleteTodo(req, res) {
-    Skill.deleteOne(req.params.id);
-    res.redirect('/skills');
-}
-
-//This is the function that adds form contents to the list
-function create(req, res) {
-    Skill.create(req.body);
-    res.redirect('/skills');
-}
-
-//create new todo function
-function newTodo(req, res) {
-    //renders the new skill page when link clicked
-    res.render('skills/new');
-}
-
-//function that will return ALL items in skills
 function index(req, res) {
     res.render('../views/skills/index', {
         skills: Skill.getAll()
     });
 }
-//function that will return a single item
 function show(req, res) {
     const skill = Skill.getOne(req.params.id);
     res.render('skills/show', { skill });
--- routes/skills.js
+++ routes/skills.js
@@ -5,9 +5,6 @@ var skillsCtrl = require('../controller/skills')
 
 /* GET users listing. */
 router.get('/', skillsCtrl.index);
-router.get('/new', skillsCtrl.new);
 router.get('/:id', skillsCtrl.show);
-router.post('/', skillsCtrl.create);
-router.delete('/:id', skillsCtrl.delete);
 
 module.exports = router;
